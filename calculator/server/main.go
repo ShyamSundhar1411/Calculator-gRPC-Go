a43a67a5a6017f48d532bd1c2f10a871cbeb146f
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-12-28 22:12:44 +0530

    Server Listeners

--- calculator/server/main.go
+++ calculator/server/main.go
@@ -1,9 +1,25 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/ShyamSundhar1411/Calculator-gRPC/calculator/proto"
+	"google.golang.org/grpc"
+)
 var address string = "0.0.0.0:5501"
 
 type Server struct{
-	
+	pb.AddServiceServer
 }
 func main(){
-
+	listener,err := net.Listen("tcp",address)
+	if err!=nil{
+		log.Fatalf("Failed listening on %v\n",err)
+	}
+	log.Printf("Listening on %s\n",address)
+	serverInstance := grpc.NewServer()
+	if err=serverInstance.Serve(listener);err!=nil{
+		log.Fatalf("Failed to listen on %v\n",err)
+	}
 }
